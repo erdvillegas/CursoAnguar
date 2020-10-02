6189a31e7362106908400a7ed536d739a1811720
Erik Villegas <erdvillegas@gmail.com>
2020-10-02 00:26:27 -0500

    Ejercicio de directivas

--- 6-miscelaneos/src/app/components/clases/clases.component.ts
+++ 6-miscelaneos/src/app/components/clases/clases.component.ts
@@ -8,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClasesComponent implements OnInit {
 
+  alerta = 'alert-danger';
+  loading = false;
+
+  // tslint:disable-next-line: ban-types
+  propiedades = {
+    danger: true
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ejecutar() {
+
+    this.loading = true;
+
+    setTimeout(() => this.loading = false, 3000);
   }
 
 }
