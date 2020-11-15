53ac7d6de6d44b3d61b3ae6067d15228097898bf
Erik Villegas <erdvillegas@gmail.com>
2020-11-15 00:48:11 -0600

    Agregando grafica de lineas

--- 16-graficos/src/app/app.module.ts
+++ 16-graficos/src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+//Graficos
+import { ChartsModule } from 'ng2-charts';
+import { LineaComponent } from './components/linea/linea.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
